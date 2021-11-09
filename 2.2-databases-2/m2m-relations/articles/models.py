61fa2155996de196c6259566c0e298d7143767d3
Sem441 <semaao@mail.ru>
2021-11-09 20:46:38 +0500

    2.2 readycheck

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,24 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tags(models.Model):
+    name = models.TextField()
+    scopes = models.ManyToManyField(Article, through="Link")
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
+
+
+class Link(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="scopes")
+    tag = models.ForeignKey(Tags, on_delete=models.CASCADE, related_name="cont")
+    is_main = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.article}_{self.tag}'
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,15 +1,15 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
     context = {}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+    students_query = Student.objects.all().prefetch_related('teachers')
 
+    context['students_query'] = students_query.order_by(ordering)
     return render(request, template, context)
