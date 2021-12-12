b0340014eaed91e06a199acfa66ed24d7e571ea8
Sem441 <semaao@mail.ru>
2021-12-12 12:40:18 +0500

    12.12 readycheck

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import request
 from rest_framework.filters import SearchFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, SAFE_METHODS
 from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
@@ -15,8 +15,12 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrViewOnly]
-    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend]
     filter_class = AdvertisementFilter
 
-    filterset_fields = ['creator__id', 'id']
-    search_field = ['id']
+    filterset_fields = ['creator']
+
+    def get_queryset(self):
+        param = self.request.query_params.get('creator')
+        if param:
+            return Advertisement.objects.filter(creator=param)
